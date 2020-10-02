e846533110608ca304e378aee6f025c3f2edb64e
Katerina Dotsenko <ekatrinadotsenko@gmail.com>
2020-10-02 20:39:11 +0200

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    if (str === '') return false;
+    str = [...str];
+    let i = 0;
+
+    while (i < str.length) {
+        bracketsConfig.forEach(arr => {
+            if (str[i] === arr[0] && str[i + 1] === arr[1]) {
+                str.splice(i, 2);
+                i = i >= 0 ? i - 2 : i;
+            };
+        });
+        i++;
+    };
+
+    return str.length === 0 ? true : false;
 }

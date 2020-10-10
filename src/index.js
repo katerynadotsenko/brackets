5b57da3e8e08aa75be2cb6dea48cf48ab712bcb1
Katerina Dotsenko <ekatrinadotsenko@gmail.com>
2020-10-10 19:02:24 +0200

    feat: add solution for task Brackets. using stack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,39 @@
 module.exports = function check(str, bracketsConfig) {
 
+    /* Solution using stack */
+
     if (str === '') return false;
+
+    let bracketsConfigObject = {};
+
+    [...bracketsConfig].forEach(item => {
+        bracketsConfigObject[item[0]] = item[1];
+    });
+
+
+    let stack = [];
+    str = [...str];
+
+    for (let i = 0; i < str.length; i++) {
+        if (bracketsConfigObject[str[i]] && bracketsConfigObject[str[i]] !== stack[stack.length - 1]) {
+            stack.push(str[i]);
+        } else if (stack.length !== 0) {
+            if (str[i] === bracketsConfigObject[stack[stack.length - 1]]) {
+                stack.pop();
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    return stack.length === 0 ? true : false;
+
+
+    /* Solution 2 */
+
+    /*if (str === '') return false;
     str = [...str];
     let i = 0;
 
@@ -15,4 +48,5 @@ module.exports = function check(str, bracketsConfig) {
     };
 
     return str.length === 0 ? true : false;
+    */
 }
